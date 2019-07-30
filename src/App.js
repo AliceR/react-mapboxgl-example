376c42048235fdcb5e8eaba726cfc7a95864715c
Alice Rühl <alice.ruehl@gmail.com>
2019-07-30 23:58:37 +0200

    Upgrade dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin()
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import Map from './Map'
@@ -11,6 +7,7 @@ import Layer from './Layer'
 import Basemap from './Basemap'
 import OpacitySlider from './OpacitySlider'
 import Checkbox from 'material-ui/Checkbox'
+
 
 class App extends Component {
   state = {
