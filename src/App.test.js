607e2888ea93856d8c25a74a11c184e3d9611360
Alice Rühl <alice.ruehl@gmail.com>
2020-07-24 16:24:15 +0200

    Mock mapbox Map in test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  Map: jest.fn(() => ({
+    on: jest.fn(),
+    flyTo: jest.fn(),
+  })),
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
